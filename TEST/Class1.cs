94e9ffe631ed511c4f15f6f109d2e75170933b97
BADR <bouachibadr@gmail.com>
2020-01-23 21:37:23 +0100

    First

---
+++ TEST/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST
+{
+    public class Class1
+    {
+        MySql.Data.MySqlClient.MySqlConnection cnx = new MySql.Data.MySqlClient.MySqlConnection("");
+
+    }
+}
